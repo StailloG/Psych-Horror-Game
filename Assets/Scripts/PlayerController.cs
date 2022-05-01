ce32ecc01d3db1eb3cee3c4daf4a3b0abdd7e96a
PetersonNormil <petersonnormil@gmail.com>
2022-05-01 18:57:43 -0400

    new scene, added a script,
    added new scene to test player movement, and playercontroller class

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /* This script will handle all the movemenet system for the player*/ 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
