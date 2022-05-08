c0d70fa4180397ea28e2ce275a004802ef67b7c6
PetersonNormil <petersonnormil@gmail.com>
2022-05-08 15:19:19 -0400

    added playermovemnt, made player into prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {  /*Class that handles all the movement of the player, will be customizable throughout development   */
-    //Start is called before the first frame update
-    void Start()
+   
+    //"CanMove" is a boolean variable that is set to true. "get" means any other script can grab it. "private set" means only inside the class can we change the boolean 
+    public bool CanMove { get; private set; } = true;
+
+    //"Header" means we can show the text in the heirachy. its a good way to label your parameters
+    //"Serialize Field" means it will show in the inspector even though it is private 
+    [Header("Movement Parameters")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float gravity = 30.0f;
+
+    //basically sensitivity and realistic looking bounds
+    //"Range " means we can force only specific values in a slider format 
+    [Header("Look Parameters")]
+    [SerializeField, Range(1, 10)] private float lookSpeedX = 2.0f;
+    [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
+    [SerializeField, Range(1, 180)] private float upperLookLimit = 80.0f;
+    [SerializeField, Range(1, 180)] private float lowerLookLimit = 80.0f;
+
+
+    private Camera playerCamera; 
+    private CharacterController characterController;
+
+    private Vector3 moveDirection;
+    private Vector2 currentInput;
+
+    private float rotationX = 0;
+
+
+
+    //awake is like Start but its even earlier
+    private void Awake()
     {
-        
+        playerCamera = GetComponentInChildren<Camera>();
+        characterController = GetComponent<CharacterController>();
+        //hide cursor and make it go to the center of screen
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(CanMove)
+        {
+            HandleMovementInput();
+            HandleMouseLook();
+            ApplyFinalMovements();
+        }
     }
+
+    private void HandleMovementInput()
+    {
+        currentInput = new Vector2(walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
+        Debug.Log(currentInput);
+        float moveDirectionY = moveDirection.y;
+        moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
+        moveDirection.y = moveDirectionY;
+    }
+
+    private void HandleMouseLook()
+    {
+        rotationX -= Input.GetAxis("Mouse Y") * lookSpeedY;
+        rotationX = Mathf.Clamp(rotationX, -upperLookLimit, lowerLookLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
+    }
+    
+     private void ApplyFinalMovements()
+    {
+        if(!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+
+           
+        }
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
 }
